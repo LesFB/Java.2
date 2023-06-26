c4ae82cd2d9ee6053f5e8995957768baf70de9d3
Les <les.floresbae@gmail.com>
2023-06-25 19:46:39 -0600

    nuevas practicas

---
+++ Colecciones/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 52552
+ *
+ */
+module Colecciones {
+}
--- POO/src/entidades/Test.java
+++ POO/src/entidades/Test.java
@@ -7,6 +7,20 @@ public class Test {
 		Empleado EvilFelipe = new Empleado ("Evil Felipe",31,"mmm@gmail.com","56987412","ABC123","346464641","Bell Boy");
 		
 		EvilFelipe.mostrarInformacion();
+		
+		//Instancia de un cliente
+		System.out.println();
+		System.out.println("instancia de un cliente");
+		Clientes Jesus = new Clientes("jesus","abcd1234",1555.00f,"1234");
+		Jesus.mostrarInfoCliente();
+		
+		Jesus.setNombre("Evil Jesus");
+		
+		System.out.println();
+		System.out.println();
+		Jesus.mostrarInfoCliente();		
 	}
+	
+	
 
 }
